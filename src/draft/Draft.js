0ef48ba221f28da1f7af3d578d5c6022d7d2a37d
Omeed Safi <osafi@pivotal.io>
2018-07-26 11:40:28 -0700

    add sassy bits
    Signed-off-by: Toby Rumans <trumans@pivotal.io>

--- src/draft/Draft.js
+++ src/draft/Draft.js
@@ -1,6 +1,13 @@
 import React from "react"
+import "./Draft.css"
 
 export const Draft = (props) => {
     const paramsFromRoutePath = props.match.params
-    return <p>Hello {paramsFromRoutePath.username}</p>
+    return (
+        <div className="Draft">
+            <p className="Draft-greeting">
+                Hello {paramsFromRoutePath.username}
+            </p>
+        </div>
+    )
 }
--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import {UntapClient} from './untap/Client';
 import axios from "axios"
 import {ServiceDiscovery} from "./ServiceDiscovery"
 import {BrowserRouter} from "react-router-dom";
+import "./styles/all.css"
 
 const serviceDiscovery = new ServiceDiscovery();
 const untapClient = new UntapClient(serviceDiscovery.untapURI(), axios)
