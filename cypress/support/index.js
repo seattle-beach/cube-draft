1927c70bc67183aa7580f40dbb43a69ddc2c5f2c
Toby Rumans <trumans@pivotal.io>
2018-07-31 14:42:23 -0700

    clean the DB before each run
    Signed-off-by: Emily Gordon <egordon@pivotal.io>

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -18,3 +18,8 @@ import './commands'
 
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
+
+
+beforeEach(function () {
+    cy.request(Cypress.env('BACKEND_URL') + "/reset_db")
+})
